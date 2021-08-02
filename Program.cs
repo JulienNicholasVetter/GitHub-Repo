d941535222e810122ee3d30ecdc183b55bf59e3c
JulienNicholasVetter <julienvetter1999@gmail.com>
2021-08-02 12:30:07 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace GitHub_Repo
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Michael Stewart");
-			Console.WriteLine("Julien Vetter");
+			 Console.WriteLine("Julien Vetter");
         }
     }
 }
